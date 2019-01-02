ab2db91fa84f11deb21fa2cca72f24713798c7c0
Travis CI User <travis@example.org>
2019-01-02 18:54:28 +0000

    build heroku 2019-01-02 18:54:28

--- client/build/precache-manifest.be67d322d97ff44220b9e53f64cc215b.js
+++ client/build/precache-manifest.be67d322d97ff44220b9e53f64cc215b.js
@@ -12,15 +12,15 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.229c360f.js"
   },
   {
-    "revision": "e498d0016b77effe5d3b",
-    "url": "/static/js/main.e498d001.chunk.js"
+    "revision": "6d295586325b5a65d7d0",
+    "url": "/static/js/main.6d295586.chunk.js"
   },
   {
     "revision": "64eb339014c6b927c7c7",
     "url": "/static/js/1.64eb3390.chunk.js"
   },
   {
-    "revision": "e498d0016b77effe5d3b",
+    "revision": "6d295586325b5a65d7d0",
     "url": "/static/css/main.b5f58112.chunk.css"
   },
   {
@@ -28,7 +28,7 @@ self.__precacheManifest = [
     "url": "/static/css/1.6b5173c7.chunk.css"
   },
   {
-    "revision": "7c4a47ae5ef7ebb91109244e047eb4d6",
+    "revision": "d4e11a56736b6d2f70595b1985ce28c9",
     "url": "/index.html"
   }
 ];
